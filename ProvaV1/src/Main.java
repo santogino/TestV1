1c997dca757a8e8749481f6e0064fa24a35e6b55
Gino Santonicola <luigisantonicola1988@gmail.com>
2024-12-13 01:51:14 +0100

    Revert "add Napoli"
    This reverts commit a26885eac5e6383f82053a9a0a528c05f0be7fe3.

--- ProvaV1/src/Main.java
+++ ProvaV1/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Ciao sono Gino!");
-        System.out.println("sono di Napoli");
+        System.out.println("Hello, World!");
     }
 }
